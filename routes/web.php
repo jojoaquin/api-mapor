1ebe88f686633b10d56c2d066064fea2f456d972
Joaquin Thiogo <92239155+smthlikeyou24@users.noreply.github.com>
2023-08-20 11:21:24 +0700

    finalll

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'Api for memarong.id -Joaquin Thiogo'
+    ]);
 });
 
 Route::get('/storage-link', function(){
